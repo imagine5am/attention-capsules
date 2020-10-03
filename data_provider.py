3e66e63e5e291627ec082d8e66d8dd1bdcf0c92e
imageine5am <theshivamsood@gmail.com>
2020-10-03 14:54:25 +0530

    Attention-capsules base

--- data_provider.py
+++ data_provider.py
@@ -187,8 +187,8 @@ def get_data(dataset,
   provider = slim.dataset_data_provider.DatasetDataProvider(
       dataset,
       shuffle=True,
-      num_readers=500,
-      common_queue_capacity=5000*2,
+      num_readers=1,
+      common_queue_capacity=5000,
       common_queue_min=batch_size)
   '''
   provider = slim.dataset_data_provider.DatasetDataProvider(
